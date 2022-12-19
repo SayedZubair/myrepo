14d47fceb60194cf18edffc38e8ad769b0091bc3
SayedZubair <sayedzubairhashimi2020>
2022-12-18 21:38:50 -0800

    version 6

--- src/Cat.java
+++ src/Cat.java
@@ -2,5 +2,6 @@ public class Cat {
     public static void main(String[] args) {
         System.out.println("nothing to add to the cat class");
         System.out.println("cats are cute too");
+        System.out.println("i love calm cats");
     }
 }
